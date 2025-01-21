4c8d1875459dccc29ef8e9cea3a1ffaa4c55863c
Gilang EA. <144137514+excaliburXD@users.noreply.github.com>
2025-01-22 01:30:51 +0700

    Update device.mk

--- device.mk
+++ device.mk
@@ -8,7 +8,7 @@
 LOCAL_PATH := device/infinix/radiant
 
 # Enable Virtual A/B OTA
-$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_vendor_ramdisk.mk)
+#$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_vendor_ramdisk.mk)
 #$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/compression.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
 
